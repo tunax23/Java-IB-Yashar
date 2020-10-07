b1760188a9cee9825a2cbc45df227b6e96b7689d
Yashar-Nishaburi <Nishaburiy@gmail.com>
2020-10-07 12:34:38 +0800

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,23 +1,8 @@
 /*
  * Mock.java
- * 
- * Copyright 2020 Yashar Nishaburi <yasharnishaburi@10-72-120-145.wifi.8.s.pas.ofs.edu.sg>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
+ * Copyright protected by me 
+ * No copying 
+ * If copy I hit 
  * 
  */
 
@@ -36,7 +21,7 @@ public class Mock
 			{
 				System.out.println("Base = zero. Nothing to do.");
 				System.exit(0);
-			}	
+			}
 			if (n2 < 0)
 			{
 				System.out.println("Warning: exponent should be positive");
@@ -60,11 +45,18 @@ public class Mock
 				System.out.println( n1 + " to the power of " + n2 + " = 1/" + ans);
 				double decans = 1.0 / ans;
 				System.out.println( "  1" );
-				System.out.println("---- = " + decans);
+				System.out.println("---- = ");
 				System.out.println( " " + n1 + "^" + n2*-1);
+				System.out.println( "  1" );
+				System.out.println("---- = " + decans );
+				System.out.println(" " + ans);
+			}
+			if (n2 == 0)
+			{
+				System.out.println("Program has ended");
+				System.exit(0);
 			}
 		}
-		System.out.println("Program ended");
 	}
 }
 
